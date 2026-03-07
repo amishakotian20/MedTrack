a9f4ccf4dc9721aa213ec1c8d715c77552fa666c
Amisha Kotian <amishakotian2005@gmail.com>
2026-03-07 21:18:18 +0530

    Update region to us-east-1

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from functools import wraps
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'medtrack-secret-key-2024')
-
-AWS_REGION = os.environ.get('AWS_REGION', 'ap-south-1')
+AWS_REGION = os.environ.get('AWS_REGION', 'us-east-1')
 
 # ─── SNS TOPIC ARN ─────────────────────────────────────────
 SNS_TOPIC_ARN = 'arn:aws:sns:ap-south-1:254652354090:MedTrack_SNS'
